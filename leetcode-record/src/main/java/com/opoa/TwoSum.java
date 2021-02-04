5596cf8d2af551d670cc8d9f7988d595ac0cce64
opoa <ridiculousme@qq.com>
2021-02-04 23:24:41 +0800

    optimize

--- leetcode-record/src/main/java/com/opoa/TwoSum.java
+++ leetcode-record/src/main/java/com/opoa/TwoSum.java
@@ -38,7 +38,6 @@ public class TwoSum {
      * 暴力解法
      */
     public static int[] solution1(int[] nums, int target) {
-
         for(int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
