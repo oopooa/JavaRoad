2eb19712a0769329b1437249e9c2693f432bbd21
opoa <ridiculousme@qq.com>
2021-02-04 23:18:06 +0800

    init and first commit

---
+++ leetcode-record/src/test/java/com.opoa/TestTwoSum.java
@@ -0,0 +1,16 @@
+package com.opoa;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestTwoSum {
+
+    @Test
+    public void testSolution1() {
+        Assert.assertArrayEquals(new int[]{0, 1}, TwoSum.solution1(new int[]{2, 7, 11, 15}, 9));
+        Assert.assertArrayEquals(new int[]{1, 2}, TwoSum.solution1(new int[]{1, 2, 3}, 5));
+        Assert.assertArrayEquals(new int[]{}, TwoSum.solution1(new int[]{5, 2, 1}, 10));
+    }
+
+}
